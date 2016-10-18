dfac41e982096dadfe2d1d1861abc1f48034a5bb
Dmitry <cheredima@ya.ru>
2016-10-18 13:05:50 +0300

    Добавил функционал

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,15 @@
 import json
-
+from pprint import pprint
 
 def load_data(filepath):
-    pass
+    with open(filepath) as json_file:
+        bars = json.load(json_file)
+    return bars
 
 
 def pretty_print_json(data):
-    pass
+    pprint(data)
 
 
 if __name__ == '__main__':
-    pass
+    pretty_print_json(load_data("file.json"))
